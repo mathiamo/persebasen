8d9aee5479eef3016c3251c8d876fc7856a619ff
mathiamo <mathiamo@gmail.com>
2024-02-20 14:41:34 +0100

    vercel stuff

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,21 @@
-import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-    return new PrismaClient()
+import { PrismaClient } from '../prisma/generated/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient();
+} else {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (!global.prisma) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        global.prisma = new PrismaClient();
+    }
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    prisma = global.prisma;
 }
 
-declare global {
-    const prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const prisma = globalThis.prisma ?? prismaClientSingleton()
-
-export default prisma
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-if (process.env.NODE_ENV !== 'production') { // @ts-ignore
-    globalThis.prisma = prisma
-}
+export default prisma;
