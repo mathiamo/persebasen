730c9481f08a072e32f09c0986863a811273f9a0
mathiamo <mathiamo@gmail.com>
2024-02-23 09:03:21 +0100

    fix update

--- pages/api/runners/update.ts
+++ pages/api/runners/update.ts
@@ -6,66 +6,59 @@ import {PersonalBest} from '../../../models/runner';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const updatedRunner = req.body;
-        console.log('Updating runner', updatedRunner)
-        const updatedRunnerData = {
-            name: updatedRunner.name,
-            age: updatedRunner.age,
-            image: updatedRunner.image,
-            personalBests: {
-                upsert: updatedRunner.personalBests.map((pb: PersonalBest) => ({
-                    where: {
-                        // Assuming a combination of properties to identify a PersonalBest
-                        distance_value: pb.distance.value,
-                        distance_unit: pb.distance.unit,
-                        time_seconds: pb.time.seconds,
-                        time_minutes: pb.time.minutes,
-                        time_hours: pb.time.hours,
-                        time_hundredths: pb.time.hundredths,
-                        location: pb.location,
-                        date: pb.date,
-                    },
-                    update: {
-                        // Update the properties as needed
-                        timeString: pb.timeString,
-                    },
-                    create: {
-                        // Create a new PersonalBest
-                        distance: {
-                            create: {
-                                value: pb.distance.value,
-                                unit: pb.distance.unit,
-                            },
-                        },
-                        time: {
-                            create: {
+
+        const updatedRunnerData = await prisma.runner.update({
+            where: {id: updatedRunner.id},
+            data: {
+                name: updatedRunner.name,
+                age: updatedRunner.age,
+                image: updatedRunner.image,
+                personalBests: {
+                    upsert: updatedRunner.personalBests.map((pb: PersonalBest) => ({
+                        where: {
+                            // Assuming a combination of properties to identify a PersonalBest
+                            distance: {value: pb.distance.value, unit: pb.distance.unit},
+                            time: {
                                 seconds: pb.time.seconds,
                                 minutes: pb.time.minutes,
                                 hours: pb.time.hours,
                                 hundredths: pb.time.hundredths,
                             },
+                            location: pb.location,
+                            date: pb.date,
                         },
-                        timeString: pb.timeString,
-                        location: pb.location,
-                        date: pb.date,
-                    },
-                })),
+                        update: {
+                            // Update the properties as needed
+                            timeString: pb.timeString,
+                        },
+                        create: {
+                            distance: {
+                                create: {
+                                    value: pb.distance.value,
+                                    unit: pb.distance.unit,
+                                },
+                            },
+                            time: {
+                                create: {
+                                    seconds: pb.time.seconds,
+                                    minutes: pb.time.minutes,
+                                    hours: pb.time.hours,
+                                    hundredths: pb.time.hundredths,
+                                },
+                            },
+                            timeString: pb.timeString,
+                            location: pb.location,
+                            date: pb.date,
+                        },
+                    })),
+                },
             },
-        };
-
-        const updatedRunnerResult = await prisma.runner.update({
-            where: {id: updatedRunner.id},
-            data: updatedRunnerData,
         });
 
-        res.json(updatedRunnerResult);
+        res.json(updatedRunnerData);
     } catch (error: unknown) {
-        if (error instanceof Error) {
-            console.error('Error:', error.message);
-            res.status(500).json({error: 'Internal Server Error'});
-        } else {
-            console.error('Unknown error:', error);
-            res.status(500).json({error: 'Internal Server Error'});
-        }
+        console.error('Error:', error);
+        res.status(500).json({error: 'Internal Server Error'});
     } finally {
         await prisma.$disconnect();
     }
